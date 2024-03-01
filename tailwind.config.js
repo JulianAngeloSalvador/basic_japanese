e456a36d121dbb4026392c5463f6f68ee0abe4b3
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-01 08:31:12 +0800

    Added Color Palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,19 +9,25 @@ export default {
       fontSize: {
         headline: "calc()",
       },
+      colors: {
+        ctaGreen: "#31E981",
+        ctaGreen2: "#20BF55",
+        ctaBlue: "#A3BCF9",
+        ctaBlue2: "#86A5D9",
+        oxford_blue: "#030027",
+        dark_purple: "#0F0326",
+      },
     },
   },
   plugins: [
     createThemes({
       dark: {
-        primary: "",
-        secondary: "",
-        cta: "",
+        primary: "#395C6B",
+        secondary: "#FFEEDB",
       },
       light: {
-        primary: "",
-        secondary: "",
-        cta: "",
+        primary: "#FFEEDB",
+        secondary: "#395C6B",
       },
     }),
   ],
