1cd50332c5c21ceed82da3c8163dbed32fa14518
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-02 19:30:49 +0800

    Fixed Header comp's layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,10 @@
+import ThemeToggler from "./ThemeToggler";
+
 export default function Header() {
   return (
-    <header>
-      <h1>KotoRaku</h1>
+    <header className="flex justify-between w-fluid-1200 mx-auto py-4 px-4 xl:px-0">
+      <h1 className=" text-main-fs">KotoRaku</h1>
+      <ThemeToggler />
     </header>
   );
 }
