63f8d9a72fc7dab69f0e52b531a54ba61eae2cad
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-02 21:56:32 +0800

    Added Auth CTA buttons & Temporarily disabled Theme Toggler comp  while evaluating options

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,17 @@ export default function Header() {
   return (
     <header className="flex justify-between w-fluid-1200 mx-auto py-4 px-4 xl:px-0">
       <h1 className=" text-main-fs">KotoRaku</h1>
-      <ThemeToggler />
+      <div className="flex items-center gap-4">
+        {/* <ThemeToggler /> */}
+        <div className="rounded-full flex items-center gap-4 children:text-sm children:font-600 children:py-1">
+          <button className="relative after:absolute after:bottom-0 after:left-0 after:w-full after:h-0.5 after:scale-x-0 after:bg-secondary hocus:after:left-0 hocus:after:scale-100 after:transition-transform after:ease-in-out after:duration-100 hocus:-translate-y-0.5 transition-transform duration-100 ease-in-out delay-75">
+            Log In
+          </button>
+          <button className=" bg-cta px-2 rounded-md text-light-theme hocus:-translate-y-0.5 transition-transform duration-100 ease-in-out delay-75">
+            Sign Up
+          </button>
+        </div>
+      </div>
     </header>
   );
 }
--- src/components/ThemeToggler.tsx
+++ src/components/ThemeToggler.tsx
@@ -1,4 +1,5 @@
 import { useThemeContext } from "../context/ThemeContext";
+import { IoSunnyOutline, IoMoon } from "react-icons/io5";
 
 export default function ThemeToggler() {
   const { setTheme, theme } = useThemeContext();
@@ -7,5 +8,20 @@ export default function ThemeToggler() {
     const newTheme = theme === "dark" ? "light" : "dark";
     setTheme(newTheme);
   };
-  return <button onClick={handleTheme}>Theme</button>;
+  return (
+    <button
+      onClick={handleTheme}
+      className="outline outline-1 outline-secondary w-9 rounded-full p-0.5 relative"
+    >
+      <div className="absolute inset-0 flex items-center justify-around px-0.5 children:text-[.95em] children:z-10 children:text-primary">
+        <div>
+          <IoSunnyOutline />
+        </div>
+        <div>
+          <IoMoon />
+        </div>
+      </div>
+      <div className="w-1/2 aspect-square rounded-full bg-secondary" />
+    </button>
+  );
 }
