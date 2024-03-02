0b90597a1c46a4ff957e7487d658a70f79a38d84
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-02 18:23:02 +0800

    Deployed Root and Error page component with react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./layout/App.tsx";
 import "./styles/index.css";
 import "unfonts.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+  { path: "/", Component: Root, errorElement: <ErrorPage /> },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
