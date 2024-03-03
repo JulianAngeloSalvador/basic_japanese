a56ea9fcb5c8a0830703ce50bf532287fc5fa7ea
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-03 21:06:46 +0800

    Transferred Modified Header comp within Landing comp

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,10 +1,8 @@
-import Header from "../components/Header";
 import Landing from "./Landing";
 
 export default function Root() {
   return (
     <>
-      <Header />
       <main>
         <Landing />
       </main>
