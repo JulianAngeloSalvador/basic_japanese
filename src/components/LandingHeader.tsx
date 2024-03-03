81bf9f9f76ed72b4a294e08870f2a1f6ad41482b
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-03 21:05:24 +0800

    Renamed Header comp > LandingHeader

--- src/components/LandingHeader.tsx
+++ src/components/LandingHeader.tsx
@@ -1,9 +1,9 @@
 import ThemeToggler from "./ThemeToggler";
 
-export default function Header() {
+export default function LandingHeader() {
   return (
-    <header className="flex justify-between w-fluid-1200 mx-auto py-4 px-4 xl:px-0">
-      <h1 className=" text-main-fs">KotoRaku</h1>
+    <header className="flex justify-between py-6 px-4 md:px-24 ">
+      <h1 className=" text-main-fs font-lexend">KotoRaku</h1>
       <div className="flex items-center gap-4">
         {/* <ThemeToggler /> */}
         <div className="rounded-full flex items-center gap-4 children:text-sm children:font-600 children:py-1">
